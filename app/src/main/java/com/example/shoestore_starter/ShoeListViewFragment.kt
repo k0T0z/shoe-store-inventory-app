a69b2ec1a9f5804255b54854117f80565fcdfa10
saifkandil <saifsalaheldeen2002@gmail.com>
2022-07-25 08:44:26 +0200

    last

--- app/src/main/java/com/example/shoestore_starter/ShoeListViewFragment.kt
+++ app/src/main/java/com/example/shoestore_starter/ShoeListViewFragment.kt
@@ -7,6 +7,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
